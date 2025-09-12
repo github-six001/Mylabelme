f21b130d7efb235b4b4b9d76981952df1b0185df
liurh <liu15983478476@163.com>
2025-09-12 19:07:47 +0800

    集成ocr版本:该版本通过本地导入三方包方式集成ocr功能

--- labelme/widgets/file_dialog_preview.py
+++ labelme/widgets/file_dialog_preview.py
@@ -53,7 +53,7 @@ class FileDialogPreview(QtWidgets.QFileDialog):
 
     def onChange(self, path):
         if path.lower().endswith(".json"):
-            with open(path, "r") as f:
+            with open(path, "r",encoding='utf-8') as f:
                 data = json.load(f)
                 self.labelPreview.setText(json.dumps(data, indent=4, sort_keys=False))
             self.labelPreview.label.setAlignment(
